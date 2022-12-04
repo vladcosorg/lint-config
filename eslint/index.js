00e7172f8833e8643c0d7f2166dfc7510563d165
chetzof <vcosvic@gmail.com>
2022-12-05 01:46:09 +0200

    fix: all around improvements

---
+++ eslint/index.js
@@ -0,0 +1,26 @@
+// @ts-check
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { defineConfig } = require('eslint-define-config')
+module.exports = defineConfig({
+  ignorePatterns: ['.eslintrc.js'],
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    './includes/eslint.js',
+    './includes/typescript-eslint.js',
+    './includes/vue.js',
+    // './includes/sonar.js',
+    './includes/import.js',
+    './includes/unused-imports.js',
+    // './includes/unicorn.js',
+    // './includes/canonical.js',
+    './includes/json.js',
+    'prettier',
+  ],
+  globals: {},
+  rules: {},
+  plugins: [],
+})
